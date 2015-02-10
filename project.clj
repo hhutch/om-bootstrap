af35e8a4d67c297b4ea5c73c7b3e32b9c5ae3ff8
Hunter Hutchinson <hunter.hutchinson@gmail.com>
2015-02-10 15:47:57 -0600

    update to latest version

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2277"]
-                 [om "0.6.5"]
+                 [org.clojure/clojurescript "0.0-2816"]
+                 [org.omcljs/om "0.8.8"]
                  [om-bootstrap "0.0.4"]
                  [secretary "1.1.0"]
                  [prismatic/dommy "0.1.2"]
                  [garden "1.2.1"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]
+  :plugins [[lein-cljsbuild "1.0.4"]
             [lein-simpleton "1.2.0"]]
 
   :source-paths ["src"]
